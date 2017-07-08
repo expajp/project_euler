a3d1163479679246fb6383083f3fcbf2ff0278db
Shu OGAWARA <expajp@gmail.com>
2017-07-08 23:48:51 +0900

    solved the problem 15

--- 15/problem-15.rb
+++ 15/problem-15.rb
@@ -2,10 +2,22 @@
 
 lattice = Array.new(21).map{Array.new(21)}
 
+# initialize
 lattice[0].fill(1)
 lattice.each do |arr|
   arr[0] = 1
 end
 
-puts lattice.to_s
+# calculate
+lattice.each_with_index do |row, i|
+  if i > 0
+    row.each_with_index do |value, j|
+      if j > 0
+        lattice[i][j] = lattice[i-1][j].to_i + lattice[i][j-1].to_i
+      end
+    end
+  end
+end
 
+# output result
+puts lattice[20][20]
