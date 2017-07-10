4791ae72e95d695451d43bb866d1d44b747e9616
Shu OGAWARA <expajp@gmail.com>
2017-07-10 23:46:31 +0900

    failed to solve problem-17

--- 17/problem-17.rb
+++ 17/problem-17.rb
@@ -2,7 +2,7 @@
 
 # until 20
 numbers = [
-  nil,
+  "",
   "one",
   "two",
   "three",
@@ -25,7 +25,7 @@ numbers = [
   "twenty"
 ]
 
-# thirty to one hundred step by 10
+# thirty to ninety step by 10
 numbers[30] = "thirty"
 numbers[40] = "forty"
 numbers[50] = "fifty"
@@ -33,4 +33,28 @@ numbers[60] = "sixty"
 numbers[70] = "seventy"
 numbers[80] = "eighty"
 numbers[90] = "ninety"
-numbers[100] = "hundred"
+
+result = 0
+999.times do |n|
+  hundreds = 100*(n+1).div(100)
+  tens = 10*(n+1).div(10)
+  ones = (n+1).modulo(10)
+
+  if hundreds > 0
+    result += numbers[hundreds.div(100)].length
+    result += "hundred".length
+    result += "and".length
+  end
+
+  if (tens+ones).between?(0,20)
+    result += numbers[tens+ones].length
+  elsif (tens+ones) <= 99
+    result += numbers[tens].length
+    result += numbers[ones].length
+  end
+
+end
+
+result += "one thousand".delete(" ").length
+
+puts result
