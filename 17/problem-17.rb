fcea94a02e824f317d1965775a5d1c8390ff6a4c
Shu OGAWARA <expajp@gmail.com>
2017-07-10 23:24:41 +0900

    initialize on problem-17

--- 17/problem-17.rb
+++ 17/problem-17.rb
@@ -1,1 +1,36 @@
 # Problem 17
+
+# until 20
+numbers = [
+  nil,
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+  "twenty"
+]
+
+# thirty to one hundred step by 10
+numbers[30] = "thirty"
+numbers[40] = "forty"
+numbers[50] = "fifty"
+numbers[60] = "sixty"
+numbers[70] = "seventy"
+numbers[80] = "eighty"
+numbers[90] = "ninety"
+numbers[100] = "hundred"
