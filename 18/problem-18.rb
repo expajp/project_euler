9aafdef30ce80e6d4d455db0802b070877556850
Shu OGAWARA <expajp@gmail.com>
2017-07-12 23:56:06 +0900

    make a heap tree

--- 18/problem-18.rb
+++ 18/problem-18.rb
@@ -1,1 +1,21 @@
 # Problem 18
+
+heap = Array.new()
+File.open("data.txt") do |file|
+  file.each_line do |line|
+    buf_array = Array.new()
+    row = line.split(" ")
+    row.each_with_index do |value, index|
+      if index != 0 && index != row.length-1
+        buf_array.push(value)
+        buf_array.push(value)
+      else
+        buf_array.push(value)
+      end
+    end
+    heap.push(buf_array)
+  end
+end
+
+heap = heap.flatten()
+puts heap.to_s
