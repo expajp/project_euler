e9cf0f314283fca435cfce31e0e15b18e424e111
Shu OGAWARA <expajp@gmail.com>
2017-07-13 00:48:28 +0900

    solved the problem-18

--- 18/problem-18.rb
+++ 18/problem-18.rb
@@ -1,21 +1,26 @@
 # Problem 18
 
-heap = Array.new()
+tree = Array.new()
 File.open("data.txt") do |file|
   file.each_line do |line|
-    buf_array = Array.new()
-    row = line.split(" ")
-    row.each_with_index do |value, index|
-      if index != 0 && index != row.length-1
-        buf_array.push(value)
-        buf_array.push(value)
-      else
-        buf_array.push(value)
-      end
-    end
-    heap.push(buf_array)
+    tree.push(line.split(" "))
   end
 end
 
-heap = heap.flatten()
-puts heap.to_s
+tree = tree.reverse
+
+answer = 0
+
+# 底から2番目の深さのデータから、順に上っていく
+# 1つ深いデータとの間の部分木は深さ1の2分木なので、最大値は簡単にわかる
+# 求めた最大値を順に2分木の根にあたる位置に代入していくと木の高さが1つずつ減っていく
+tree.each_with_index do |depth, i|
+  if i > 0
+    depth.each_with_index do |value, j|
+      depth[j] = depth[j].to_i + (tree[i-1][j] > tree[i-1][j+1] ? tree[i-1][j].to_i : tree[i-1][j+1].to_i)
+    end
+    answer = depth.max
+  end
+end
+
+puts answer
