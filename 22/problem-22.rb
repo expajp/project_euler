6f21952b2f11bc96379f48ee4e0f6f201146aa90
Shu OGAWARA <expajp@gmail.com>
2017-07-23 00:17:21 +0900

    solved the problem-22

--- 22/problem-22.rb
+++ 22/problem-22.rb
@@ -1,1 +1,21 @@
 # Problem 22
+
+def get_score(name, index)
+  name_score = 0
+  name.each_char{ |c| name_score = name_score + @alphabet_score[c].to_i}
+  return name_score.to_i * index.to_i
+end
+
+@alphabet_score = Hash.new
+[*'A'..'Z'].each_with_index{ |c, i| @alphabet_score[c] = i.to_i + 1 }
+
+
+names = []
+File.open("names.txt") do |file|
+  names = file.read.delete("\"").split(",").sort
+end
+
+score = 0
+names.each_with_index { |name, index| score = score + get_score(name, index.to_i+1) }
+
+puts score.to_s
