f20f60140b0e4e029cf5e3605c940934349d5fcb
Shu OGAWARA <expajp@gmail.com>
2017-07-25 21:52:37 +0900

    listed up abundant numbers

--- 23/problem-23.rb
+++ 23/problem-23.rb
@@ -1,1 +1,30 @@
 # Problem 23
+
+def list_divisor(n)
+  upper_limit = Math.sqrt(n)
+  list = [1]
+
+  2.step(upper_limit, 1) do |i|
+    if n % i == 0
+      list.push(i)
+      list.push(n/i)
+    end
+  end
+
+  return list.uniq.sort
+end
+
+def func_d(n)
+  return list_divisor(n).inject(:+)
+end
+
+inabundant = []
+
+#28123
+100.times do |n|
+  if func_d(n+1) > n+1
+    inabundant.push(n+1)
+  end
+end
+
+puts inabundant.to_s
