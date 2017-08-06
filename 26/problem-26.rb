98ec77f727865070bdb24ad86cd5f593a509c4d3
Shu OGAWARA <expajp@gmail.com>
2017-08-06 23:09:53 +0900

    I have struggled with the problem-26 yet.

--- 26/problem-26.rb
+++ 26/problem-26.rb
@@ -3,23 +3,23 @@
 
 require "bigdecimal"
 
-def recurring_cycle(n, f)
+def recurring_cycle(n, f, max)
   # 一度小数点を消して整数にすると先頭の0が消える
   str = f.to_s.delete('.').to_i.to_s
 
   cycle = n
   
-  #catch(:detect) do
-    n.step(1, -1) do |len|
+  catch(:detect) do
+    n.step(max, -1) do |len|
       0.step((n/2).floor-1) do |start|
         if str.slice(start, len) == str.slice(start+len, len) && len < cycle
           cycle = len
           # break
-          #throw :detect
+          throw :detect
         end
       end
     end
-    #end
+  end
 
     puts n.to_s + " " + str + " " + cycle.to_s
     
@@ -28,10 +28,11 @@ end
 
 answer = [0, 0] # answer, length
 bd_1 = BigDecimal("1")
+cycle = 1
 
-31.step(39) do |n|
+2.step(49) do |n|
   bd_n = BigDecimal(n.to_s)
-  cycle = recurring_cycle(n.to_i, bd_1.div(bd_n, 200))
+  cycle = recurring_cycle(n.to_i, bd_1.div(bd_n, 200), cycle.to_i)
   if cycle.to_i > answer[1].to_i
     puts n.to_s + " " + cycle.to_s
     answer = [n, cycle]
