ac59d11254d73ef686598d0f6471b1cc8cf0cb08
Shu OGAWARA <expajp@gmail.com>
2017-08-07 23:49:10 +0900

    reset the function

--- 26/problem-26.rb
+++ 26/problem-26.rb
@@ -3,36 +3,26 @@
 
 require "bigdecimal"
 
-def recurring_cycle(n, f, max)
+def recurring_cycle(n)
+  bd_1 = BigDecimal("1")
+  f = bd_1.div(n, 50)
+  
   # 一度小数点を消して整数にすると先頭の0が消える
   str = f.to_s.delete('.').to_i.to_s
 
   cycle = n
   
-  catch(:detect) do
-    n.step(max, -1) do |len|
-      0.step((n/2).floor-1) do |start|
-        if str.slice(start, len) == str.slice(start+len, len) && len < cycle
-          cycle = len
-          # break
-          throw :detect
-        end
-      end
-    end
-  end
-
-    puts n.to_s + " " + str + " " + cycle.to_s
+  puts n.to_s + " " + str + " " + cycle.to_s
     
-    return cycle
+  return cycle
 end
 
 answer = [0, 0] # answer, length
-bd_1 = BigDecimal("1")
 cycle = 1
 
 2.step(49) do |n|
   bd_n = BigDecimal(n.to_s)
-  cycle = recurring_cycle(n.to_i, bd_1.div(bd_n, 200), cycle.to_i)
+  cycle = recurring_cycle(n.to_i)
   if cycle.to_i > answer[1].to_i
     puts n.to_s + " " + cycle.to_s
     answer = [n, cycle]
