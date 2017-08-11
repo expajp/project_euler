3b2322ceca538eb661fd1028376b9a580b30a4b2
Shu OGAWARA <expajp@gmail.com>
2017-08-11 23:44:39 +0900

    failed to solved the problem-27

--- 27/problem-27.rb
+++ 27/problem-27.rb
@@ -18,13 +18,55 @@ def check_if_prime(n)
   return true
 end
 
+def euclidean(a, b)
+  if a > b && b != 0
+    euclidean(a%b, b)
+  elsif a < b && a != 0
+    euclidean(a, b%a)
+  else
+    return (a > b ? a : b)
+  end
+end
+
 answer = 0
 
 1.step(1000, 1) do |a|
   1.step(1000, 2) do |b|
-  
+    next if euclidean(a, b) == 1
+
+    # a:+, b:+
     1.step do |n|
       if !check_if_prime(func(a,b,n))
+        puts "n = " + n.to_s + ", a = " + a.to_s + ", b = " + b.to_s
+        answer = (answer < n ? n : answer)
+        break
+      end
+    end
+
+    # a:-, b:+
+    1.step do |n|
+      break if func(-a, b, n) < 2
+      if !check_if_prime(func(-a,b,n))
+        puts "n = " + n.to_s + ", a = " + a.to_s + ", b = " + b.to_s
+        answer = (answer < n ? n : answer)
+        break
+      end
+    end
+
+    # a:-, b:-
+    1.step do |n|
+      break if func(-a, -b, n) < 2
+      if !check_if_prime(func(-a,-b,n))
+        puts "n = " + n.to_s + ", a = " + a.to_s + ", b = " + b.to_s
+        answer = (answer < n ? n : answer)
+        break
+      end
+    end
+
+    # a:+, b:-
+    1.step do |n|
+      break if func(a, -b, n) < 2
+      if !check_if_prime(func(a,-b,n))
         puts "n = " + n.to_s + ", a = " + a.to_s + ", b = " + b.to_s
         answer = (answer < n ? n : answer)
         break
