a8717cdbd481a5df66c988dd26d04432a8b89988
Shu OGAWARA <expajp@gmail.com>
2017-08-15 20:59:29 +0900

    interrepted by last stop

--- 31/problem-31.rb
+++ 31/problem-31.rb
@@ -1,1 +1,17 @@
 # Problem 31
+
+@coins = [1, 2, 5, 10, 20, 50, 100, 200]
+@coins = @coins.reverse
+
+def coin_pick(max_value, limit)
+  
+end
+
+limit = 200
+answer = 0
+
+(@coins.length).times do |i|
+  
+end
+
+# puts @coins.to_s
