7a1f24b88729db28fd11f513676f7058839bd4b8
Shu OGAWARA <expajp@gmail.com>
2017-08-16 23:18:57 +0900

    thinking of the problem-30

--- 31/problem-31.rb
+++ 31/problem-31.rb
@@ -3,15 +3,25 @@
 @coins = [1, 2, 5, 10, 20, 50, 100, 200]
 @coins = @coins.reverse
 
-def coin_pick(max_value, limit)
+@answer = 0
+
+def coin_pick(max_index, limit)
+  if limit == 0 || max_index >= @coins.length
+    @answer = @answer + 1
+    puts ""
+    return
+  end
   
+  max_num = limit.div(@coins[max_index])
+  max_num.step(0, -1) do |num|
+    print @coins[max_index].to_s + "*" + num.to_s + " "
+    coin_pick(max_index.to_i+1, limit.to_i-@coins[max_index]*num)
+  end
 end
 
-limit = 200
-answer = 0
+limit = 10
+coin_pick(4, limit)
 
-(@coins.length).times do |i|
-  
-end
+puts @answer.to_s
 
 # puts @coins.to_s
