1c3aebafa36ec3f66b5df0fc53495a28db4ec3b8
Shu OGAWARA <expajp@gmail.com>
2017-08-17 19:39:22 +0900

    solved the problem-31

--- 31/problem-31.rb
+++ 31/problem-31.rb
@@ -6,22 +6,20 @@
 @answer = 0
 
 def coin_pick(max_index, limit)
-  if limit == 0 || max_index >= @coins.length
+  if max_index == @coins.length-1
     @answer = @answer + 1
-    puts ""
+    # puts "#{@coins[max_index]}*#{limit} "
     return
   end
-  
+
   max_num = limit.div(@coins[max_index])
   max_num.step(0, -1) do |num|
-    print @coins[max_index].to_s + "*" + num.to_s + " "
+    # print "#{@coins[max_index]}*#{num} "
     coin_pick(max_index.to_i+1, limit.to_i-@coins[max_index]*num)
   end
 end
 
-limit = 10
-coin_pick(4, limit)
+limit = 200
+coin_pick(0, limit)
 
 puts @answer.to_s
-
-# puts @coins.to_s
