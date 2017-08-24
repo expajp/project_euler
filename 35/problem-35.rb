f8c11dbc37a8b959d1769aa311aa431dd9c0f3fb
Shu OGAWARA <expajp@gmail.com>
2017-08-24 22:05:25 +0900

    implement functions:rotate, circular

--- 35/problem-35.rb
+++ 35/problem-35.rb
@@ -1,1 +1,17 @@
 # Problem 35
+
+def circular(n)
+  ret = []
+  buf = n
+  (n.to_s.length).times do |i|
+    buf = rotate(buf)
+    ret.push(buf)
+  end
+  return ret
+end
+
+def rotate(n)
+  return n.to_s.slice(1, n.to_s.length-1) + n.to_s.slice(0, 1)
+end
+
+puts circular(1234).to_s
