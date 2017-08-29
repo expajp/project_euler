af6b2f0ce0e69a9a20e9c2eb536ba2a38787dc1e
Shu OGAWARA <expajp@gmail.com>
2017-08-29 09:03:39 +0900

    solved the problem-39 and create the files for the problem-40

--- 39/problem-39.rb
+++ 39/problem-39.rb
@@ -1,1 +1,34 @@
 # Problem 39
+
+answer = 0
+answer_stack = []
+answer_p = 0
+
+3.step(1000) do |p|
+  stack = []
+
+  # a <= b <= c
+  1.step((p/3).floor) do |a|
+
+    b = a
+    loop do
+      c = p-a-b
+      if a*a + b*b == c*c
+        stack.push([a, b, c])
+      end
+
+      b = b + 1
+      break if p-a-b < b
+    end
+  end
+
+  if answer < stack.length
+    answer = stack.length
+    answer_stack = stack
+    answer_p = p
+  end
+end
+
+puts answer.to_s
+puts answer_stack.to_s
+puts answer_p.to_s
