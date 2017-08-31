6686bf84dd9116c35381613468ee79da2c91c404
Shu OGAWARA <expajp@gmail.com>
2017-08-31 09:08:34 +0900

    struggle for the problem 41

--- 41/problem-41.rb
+++ 41/problem-41.rb
@@ -1,1 +1,38 @@
 # Problem 41
+
+def check_if_prime(n)
+  return false if n <= 1
+  return true if n == 2
+  return false if n != 2 && n % 2 == 0
+ 
+  limit = Math.sqrt(n).ceil
+  3.step(limit, 2) do |i|
+    return false if n % i == 0
+  end
+
+  return true
+end
+
+def pandigital(n, digit)
+  arr = []
+  n.to_s.each_char{|c| arr.push(c)}
+
+  arr = arr.sort
+  numbers = ('1'..digit.to_s).to_a
+  return true if arr == numbers
+  return false
+end
+
+answer = 0
+
+8.step(9) do |n|
+  (10**(n-1)+1).step(10**(n-1)*(n+1), 2) do |i|
+    next if i.to_s.include?('0')
+    next if !pandigital(i, n)
+    next if !check_if_prime(i)
+    puts i.to_s
+    answer = i
+  end
+end
+
+puts answer.to_s
