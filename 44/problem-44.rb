b529eebcd39c24b69d0431241bb218af860d4b3c
Shu OGAWARA <expajp@gmail.com>
2017-09-06 09:04:28 +0900

    struggle to solve the problem-44

--- 44/problem-44.rb
+++ 44/problem-44.rb
@@ -12,7 +12,6 @@ def add_pentagonal(start, length, arr)
   start.step(start+length) do |n|
     arr.push(n*(3*n-1)/2)
   end
-
   return arr  
 end
 
@@ -21,17 +20,28 @@ def pentagonal?(m)
   # 3n^2 - n - 2m = 0
   # この方程式が成立する整数nがあるかどうか？という問題を解かないとだめ
   # y = 3n^2 - n はn>=1において単調増加なので、
-  # nを順番に代入していって2mより大きくなった時点でストップ、
-  # という原始的な方法でOK
-  
-end
-  
-pentagonal = add_pentagonal(1,10,[])
-
-pentagonal.length.times do |i|
-  (i+1).step(pentagonal.length-1) do |j|
-    
+  # nを順番に代入していって2mより大きくなった時点でストップ
+  n = 1
+  loop do
+    y = 3*n*n-n
+    return true if y == 2*m
+    return false if y > 2*m
+    n = n + 1
   end
+  
 end
 
-puts pentagonal.to_s
+catch(:first_search) do
+  p = add_pentagonal(1,100,[])
+
+  p.length.times do |i|
+    (i+1).step(p.length-1) do |j|
+      # puts "#{i}, #{p[i]} #{j}, #{p[j]} #{pentagonal?(p[i]+p[j])} #{pentagonal?(p[j]-p[i])}"
+      if pentagonal?(p[i]+p[j]) && pentagonal?(p[j]-p[i])
+        puts "#{p[i]} #{p[j]}"
+        throw(:first_search)
+      end
+    end
+  end
+
+end
