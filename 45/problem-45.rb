6e1de15691e96aed558ceeec1c97c720a4b65a39
Shu OGAWARA <expajp@gmail.com>
2017-09-11 09:01:27 +0900

    failed to solve the problem 45

---
+++ 45/problem-45.rb
@@ -0,0 +1,45 @@
+# Problem 45
+
+def pentagonal?(m)
+  n = 1
+  loop do
+    y = 3*n*n-n
+    return true if y == 2*m
+    return false if y > 2*m
+    n = n + 1
+  end
+  
+end
+
+def triangle?(m)
+  n = 1
+  loop do
+    y = n*n+n
+    return true if y == 2*m
+    return false if y > 2*m
+    n = n + 1
+  end
+  
+end
+
+def get_hexagonal(start, length)
+  ret = []
+  start.step(start+length) do |n|
+    ret.push(n*(2*n-1))
+  end
+  return ret.sort
+end
+
+start = 144
+answer = 0
+h = get_hexagonal(start, 20000)
+
+h.each do |h_x|
+  next if !pentagonal?(h_x)
+  puts h_x.to_s
+  next if !triangle?(h_x)
+  answer = h_x
+  break
+end
+
+puts answer.to_s
