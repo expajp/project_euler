c5da3f603ee1a91fcf60084ed84fb2cf1d47eb0f
Shu OGAWARA <expajp@gmail.com>
2018-02-20 20:40:14 +0900

    solved the problem 53

--- 53/problem-53.rb
+++ 53/problem-53.rb
@@ -10,3 +10,33 @@
 n = 23 になるまで, これらの値が100万を超えることはない: 23C10 = 1144066.
 1 ≤ n ≤ 100 について, 100万を超える nCr は何通りあるか?
 =end
+
+def fact(n)
+  [*1..n].inject(&:*)
+end
+
+def fact_top_m(n, m)
+  [*(n-m+1)..n].inject(&:*)
+end
+
+def num_combination(n, r)
+  fact_top_m(n, r)/fact(r)
+end
+
+n = 23
+r = 10
+
+answer = 0
+23.upto(100) do |n|
+  flg = false
+  num = 0
+  2.upto(n) do |r|
+    num = num_combination(n, r)
+    next if num <= 1000000
+    break if num <= 1000000 && flg
+    flg = true
+    answer += 1
+  end
+end
+
+p answer.to_s
