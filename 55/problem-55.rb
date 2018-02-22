711b1e384e98cab27ed9bdbb343830c73add6f86
Shu OGAWARA <expajp@gmail.com>
2018-02-23 02:03:35 +0900

    solved the problem-55

--- 55/problem-55.rb
+++ 55/problem-55.rb
@@ -28,3 +28,22 @@ def palindromic_num?(num)
   return true if num.to_s == num.to_s.reverse
   false
 end
+
+def sum_up_with_reverse(num)
+  num + num.to_s.reverse.to_i
+end
+
+def lychrel?(num)
+  buf = num
+  49.times do |i|
+    return false if palindromic_num?(buf = sum_up_with_reverse(buf))
+  end
+  true
+end
+
+answer = 0
+1.upto(9999) do |i|
+  answer += 1 if lychrel?(i)
+end
+
+p answer
