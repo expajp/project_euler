3a69915b786eedecdc18117a571887206bf194a9
Shu OGAWARA <expajp@gmail.com>
2018-02-26 03:22:20 +0900

    create methods of class

--- 57/problem-57.rb
+++ 57/problem-57.rb
@@ -17,8 +17,8 @@
 最初の1000項を考えたとき, 分子の桁数が分母の桁数を超える項はいくつあるか?
 =end
 
-class fraction
-  attr_accessor: :element, :denominator
+class Fraction
+  attr_accessor :element, :denominator
 
   def initialize(elm, den)
     @element = elm
@@ -26,10 +26,34 @@ class fraction
   end
 
   # 通分
-  def reduction
+  def reduce_to_common_den
   end
-  
+
+  # 分数表示
+  def to_s
+    "#{@element}/#{@denominator}"
+  end
+
+  # 加算
+  def add
+  end
+
+  private
+
+  # 約分
+  def reduce
+  end
+
+  # 分母と分子の最大公約数を求める
+  def get_max_common_divisor(m, n)
+    
+  end
+
+  # 約数を求める
+  def get_divisors(n)
+  end  
 end
 
 # 工数を指定して再帰的に求める
-
+frac = Fraction.new(1,2)
+p frac.to_s
