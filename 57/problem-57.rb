4f986c3a4571f0983bb7b3b038c8d1d3ac6030ca
Shu OGAWARA <expajp@gmail.com>
2018-02-28 23:36:05 +0900

    failed to solve the problem for performance

--- 57/problem-57.rb
+++ 57/problem-57.rb
@@ -21,6 +21,7 @@ class Fraction
   attr_accessor :elm, :den
 
   def initialize(elm, den)
+    raise if den == 0
     @elm = elm
     @den = den
   end
@@ -34,6 +35,20 @@ class Fraction
   def add(frac)
     @elm = @elm*frac.den + frac.elm*@den
     @den = @den * frac.den
+    reduce()
+    self
+  end
+
+  # 逆数
+  def inverse
+    Fraction.new(@den, @elm)
+  end
+  
+  # 逆数（破壊的メソッド）
+  def inverse!
+    buf = @elm
+    @elm = @den
+    @den = buf
     self
   end
 
@@ -41,23 +56,49 @@ class Fraction
 
   # 約分
   def reduce
-    # self
+    max_common_div = get_max_common_divisor(@elm, @den)
+    @elm = @elm / max_common_div
+    @den = @den / max_common_div
   end
 
   # 分母と分子の最大公約数を求める
   def get_max_common_divisor(m, n)
-    
+    (get_divisors(m) & get_divisors(n)).max
   end
 
   # 約数を求める
   def get_divisors(n)
-    
+    stop = Math.sqrt(n).ceil
+    ret = [1, n]
+    2.upto(stop) do |i|
+      ret.push(i).push(n/i) if n%i == 0      
+    end
+    ret.uniq.sort
   end  
 end
 
-# 工数を指定して再帰的に求める
-frac = Fraction.new(1,6)
-frac_2 = Fraction.new(1,3)
-p frac.to_s
-p frac_2.to_s
-p frac.add(frac_2).to_s
+def get_approximation(n)
+  return Fraction.new(0,1) if n < 1
+  return Fraction.new(1,1).add(get_denomi(n).inverse)
+end
+
+@denomi = [0, Fraction.new(2,1)]
+
+def get_denomi(n)
+  return 0 if n < 1
+  return @denomi[1] if n == 1
+  return @denomi[n] if !(@denomi[n].nil?)
+  ret = Fraction.new(2,1).add(get_denomi(n-1).inverse)
+  @denomi[n] = ret
+  ret
+end
+
+answer = 0
+1.upto(1000) do |i|
+  apx = get_approximation(i)
+  p @denomi.map(&:to_s)
+  p apx.to_s
+  answer += 1 if apx.den.to_s.length != apx.elm.to_s.length
+  # p "i = #{i}" if i % 10 == 0
+end
+p answer
