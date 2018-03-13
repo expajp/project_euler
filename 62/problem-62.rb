5727b52f628bd8106131d434f39e61d441310a99
Shu OGAWARA <expajp@gmail.com>
2018-03-14 00:24:01 +0900

    implement but too slow

--- 62/problem-62.rb
+++ 62/problem-62.rb
@@ -10,6 +10,7 @@
 立方数かどうかの判定は、その数の３乗根に天井関数を適用
 =end
 
+=begin
 def cube?(n)
   m = 1
   loop do
@@ -18,12 +19,25 @@ def cube?(n)
     m += 1
   end
 end
+=end
+@cube = []
+def set_cubes(max)
+  1.upto(max){ |n| @cube.push(n**3) }
+end
+
+def cube?(n)
+  @cube.include?(n)
+end
 
 def permutation_numbers(n)
   arr = n.to_s.split('').permutation(n.to_s.length).to_a.uniq
-  p arr
   arr.map{ |a| a.inject(:+).to_i }
 end
 
-# [*1..64].each{ |n| p "#{n} #{cube?(n)}" }
-p permutation_numbers(122)
+set_cubes(1000)
+n = 5
+loop do
+  p "n = #{n}, n^3 = #{n**3}"
+  break if permutation_numbers(n**3).select{ |i| cube?(i) }.count == 5
+  n += 1
+end
