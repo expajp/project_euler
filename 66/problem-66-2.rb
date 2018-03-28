fab80958c56c047ac1bcff924786ba8820c2d31a
Shu OGAWARA <expajp@gmail.com>
2018-03-28 22:31:52 +0900

    start to use a convergent

---
+++ 66/problem-66-2.rb
@@ -0,0 +1,85 @@
+# coding: utf-8
+# Problem 66
+
+=begin
+次の形式の, 2次のディオファントス方程式を考えよう:
+
+x^2 - Dy^2 = 1
+たとえば D=13 のとき, x を最小にする解は 649^2 - 13×18^2 = 1 である.
+
+D が平方数(square)のとき, 正整数のなかに解は存在しないと考えられる.
+
+D = {2, 3, 5, 6, 7} に対して x を最小にする解は次のようになる:
+
+3^2 - 2*2^2 = 1
+2^2 - 3*1^2 = 1
+9^2 - 5*4^2 = 1
+5^2 - 6*2^2 = 1
+8^2 - 7*3^2 = 1
+したがって, D ≤ 7 に対して x を最小にする解を考えると, D=5 のとき x は最大である.
+
+D ≤ 1000 に対する x を最小にする解で, x が最大になるような D の値を見つけよ.
+
+---
+この方程式はペル方程式というらしく、Wikipediaに項目がある
+底を連分数展開し、周期が決まったところで分数近似する
+二乗して分子をx, 分母をyとおくとそれが最小解である
+
+=end
+
+def get_int_part_of_sqrt(n)
+  Math.sqrt(n).floor.to_i  
+end
+
+def square?(n)
+  (Math.sqrt(n).floor.to_i)**2 == n
+end
+
+def get_into_rational(root, int, elm)
+  denomi = (root - int*int) / elm
+  left = (get_int_part_of_sqrt(root)+int)/denomi
+  new_int = (denomi*left  - int).abs
+
+  return { "left": left, "denomi": denomi, "root": root, "int": new_int } # left, denomi, root, int
+end
+
+def get_str_hash(h)
+  "#{h[:left]} + (sqrt(#{h[:root]})-#{h[:int]}) / #{h[:denomi]}"
+end
+
+def get_convergent(arr)
+  answer = { "elm": 1, "denomi": 0 }
+  rev = arr.reverse
+  rev.each do |i|
+    answer = { "elm": answer[:denomi]+answer[:elm]*i, "denomi": answer[:elm] }
+  end
+  answer
+end
+
+def get_str_convergent(answer)
+  "#{answer[:elm]}/#{answer[:denomi]}"
+end
+
+answer = 0
+n = 2
+loop do
+  h = {"root": n, "int": get_int_part_of_sqrt(n), "denomi": 1}
+  
+  arr = []
+  log = []
+  loop do
+    previous_h = h.dup
+    h = get_into_rational(h[:root], h[:int], h[:denomi])
+    if log.any?{ |log| log == h }
+      p "n=#{n}, #{arr}"
+      p get_str_convergent(get_convergent(arr))
+      break
+    end
+    arr.push(h[:left])
+    log.push(h.dup)
+  end
+  
+  n += 1
+  n += 1 if square?(n) # 次の数が平方数ならスキップ
+  break if n > 10
+end
