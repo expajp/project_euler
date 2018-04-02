ec9e027b783a6f0d711af8d848496393ab5423c5
Shu OGAWARA <expajp@gmail.com>
2018-04-03 00:32:18 +0900

    solved but that was not max

--- 68/problem-68.rb
+++ 68/problem-68.rb
@@ -25,18 +25,33 @@ p_068_1.gif
 (注, 3つの場合の例を見ても分かる通り, 列の始まりの数字を比べた時一番小さい数字で始まる列から時計回りに繋げるという条件のもとで文字列を生成する必要があります. この条件下で最大となる数字を答えてください. )
 =end
 
-max = 6
-nums = [*1..max]
-first_two = nums.combination(2).to_a
-
-first_two.each do |ft|
-  rest = nums.dup-ft
-  ft.each do |n|
-    rest.each do |m|
-      inner = [(ft-[n]).first, m]
-      outer = [n]
-      sum = ft.sum + m
-      p "inner = #{inner}, outer = #{outer}, sum = #{sum}"
-    end
+def output(inner, outer)
+  len = inner.length
+  str = ""
+  len.times do |i|
+    str += " #{outer[i]},#{inner[i]},#{inner[(i+1)%len]};"
   end
+  p (outer[0] + inner[0] + inner[1]).to_s + str
+end
+
+def check(inner, outer)
+  arr = []
+  len = inner.length
+  len.times do |i|
+    sum = outer[i] + inner[i] + inner[(i+1)%len]
+    return if arr.length != 0 && arr.last != sum
+    arr.push(sum)
+  end
+  output(inner, outer)
+end
+
+max = 10
+nums = [*1..max]
+outers = nums.combination(max/2).to_a # outerをcombinationで取ることで同じ和の最小値のみを出力
+
+outers.each do |outer|
+  inners = (nums.dup - outer).permutation(max/2)
+  inners.each do |inner|
+    check(inner, outer)
+  end 
 end
