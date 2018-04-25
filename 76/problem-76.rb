1755307653dcd281b2fad69a821dcc9a89cda2b5
Shu OGAWARA <expajp@gmail.com>
2018-04-26 00:53:19 +0900

    success to shorten

--- 76/problem-76.rb
+++ 76/problem-76.rb
@@ -14,36 +14,19 @@
 2つ以上の正整数の和としての100の表し方は何通りか.
 =end
 
-@patterns = [nil, 0]
 @gpfnah = []
 
 # get patterns from n and h
 def gpfnah(n, h)
   return @gpfnah[n][h] unless @gpfnah[n].nil? || @gpfnah[n][h].nil?
   return 1 if h == 1
-  ret = 0
-  [*1..n/h].each do |i|
-    [*1..h-1].each do |j|
-      ret += gpfnah(n-h*i, h-j)
-    end
-  end
   @gpfnah[n] = [] if @gpfnah[n].nil?
-  @gpfnah[n][h] = ret
-  return ret
+  @gpfnah[n][h] = [*1..n/h].map{ |i| [*1..h-1].map{ |j| gpfnah(n-h*i, h-j) }.sum }.sum
 end
 
 # snp = sum notation patterns
 def snp(n)
-  return @patterns[n] unless @patterns[n].nil?
-  @patterns[n] = 0
-  [*1..(n/2).floor].each do |i|
-    @patterns[n] += snp(i)+1
-  end
-  [*(n/2).floor+1..n-1].each do |i|
-    pt = gpfnah(n, n-i)
-    @patterns[n] += pt
-  end
-  @patterns[n]
+  [*1..n-1].map{ |i| gpfnah(n, n-i)}.sum
 end
 
 p snp(100)
