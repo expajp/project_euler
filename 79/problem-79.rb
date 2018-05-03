d6419bd1b244672ad9625e44c1f8926a3dd735d8
Shu OGAWARA <expajp@gmail.com>
2018-05-03 17:01:50 +0900

    read keylog

---
+++ 79/problem-79.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+# Problem 79
+
+=begin
+オンラインバンクで通常使われるsecurity methodは, 
+パスコードからランダムに選んだ3文字をユーザーに要求するものである.
+
+たとえば, パスコードが531278のとき, 2番目, 3番目, 5番目の文字を要求されるかもしれない. 
+このとき, 期待される答えは: 317 である.
+
+テキストファイルkeylog.txtには, ログインに成功した50回の試行が記録されている.
+
+3つの文字が常に順番通りに要求されるとするとき, 
+ファイルを分析して, 可能なパスコードのなかでもっとも短いものを見つけよ.
+=end
+
+keylog = []
+File.open("keylog.txt") do |f|
+  f.each_line do |line|
+    line.gsub!(/\n/, "")
+    arr = []
+    line.each_char{ |c| arr << c }
+    keylog << arr
+  end
+end
+p keylog
