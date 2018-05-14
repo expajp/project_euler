a00a556bed5f240e2bb3da9246c72f4ce4d12d0b
Shu OGAWARA <expajp@gmail.com>
2018-05-14 09:33:18 +0900

    solved the problem 80

--- 80/problem-80.rb
+++ 80/problem-80.rb
@@ -12,3 +12,14 @@
 =end
 
 require 'bigdecimal/math'
+
+def square?(n)
+  (Math.sqrt(n).floor)**2 == n
+end
+
+sum = 0
+2.upto(99) do |i|
+  next if square?(i)
+  sum += BigMath::sqrt(BigDecimal.new(i), 90).to_s[2..-3][0..99].split('').map(&:to_i).inject(:+)
+end
+p sum
